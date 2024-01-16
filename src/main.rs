b53bf331b88fbf1e96b2493398b62d19e620e66d
AndrewDanial <adanial9379@gmail.com>
2024-01-16 15:53:13 -0500

    Hello World written to the screen

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@
 #![no_main] // disable all rust-level netry points
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't mangle the name of this function (keep it as _start)
 pub extern "C" fn _start() -> ! {
     // this is the entry point that the linker looks for
     // named _start by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
-
 
 // this function is called on panic
 #[panic_handler]
