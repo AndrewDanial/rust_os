0f5508222d1aa7b638a861e3e48cbcc503c591e7
AndrewDanial <adanial9379@gmail.com>
2024-01-16 16:47:11 -0500

    Printing abstractions such as the println! macro

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
 #![no_std] // don't link the rust std
 #![no_main] // disable all rust-level netry points
-use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+
+use core::panic::PanicInfo;
 
 #[no_mangle] // don't mangle the name of this function (keep it as _start)
 pub extern "C" fn _start() -> ! {
     // this is the entry point that the linker looks for
     // named _start by default
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("panic gaming");
     loop {}
 }
 
 // this function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
