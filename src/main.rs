809d3571b291de2c9576245d0843ca90b1369822
AndrewDanial <adanial9379@gmail.com>
2024-01-16 21:22:31 -0500

    Can now write tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std] // don't link the rust std
 #![no_main] // disable all rust-level netry points
-
+#![feature(custom_test_frameworks)]
+#![test_runner(rustos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -10,13 +13,22 @@ pub extern "C" fn _start() -> ! {
     // this is the entry point that the linker looks for
     // named _start by default
     println!("Hello World{}", "!");
-    panic!("panic gaming");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
 // this function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rustos::test_panic_handler(info)
 }
